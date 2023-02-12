36312068cf549e18711a98d81e185a147640baae
Viktor-Penkov <124812344+Viktor-Penkov@users.noreply.github.com>
2023-02-12 12:45:59 +0200

    Add files via upload
    add files

---
+++ js/1.js
@@ -0,0 +1,14 @@
+let salary = prompt("Введіть розмір вашої зарплати")
+console.log(salary);
+const premium = salary * 0.15 ;
+console.log(premium .toFixed(2));
+const tax = (salary +++ premium) * 0.1;
+console.log(tax .toFixed(2));
+const outlay = 190;
+console.log(outlay)
+const res = salary + premium - tax - outlay - 1
+console.log(res .toFixed(2))
+
+alert("Ваш залишок коштів:" + res);
+
+
